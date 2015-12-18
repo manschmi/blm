fd73ceecea14906090826f3d8b98dc398ff52103
manschmi <ms@mbg.au.dk>
2015-12-18 15:28:31 +0100

    added qqplot

--- R/blm.R
+++ R/blm.R
@@ -806,11 +806,34 @@ resid_vs_fitted_plot <- function(object, show_var=FALSE, ...) {
 #' @param object a \code{\link{blm}} object.
 #' @param ... other arguments passed to plot.
 #'   
-#' @detail Scale-location plots the sqrt(| residuals |) against fitted values. This is supposed to diminish skewness.
+#' @detail Scale-location plots the sqrt(| residuals |) against fitted values. This is supposed to diminish skewness. Note: In contrast to the \code{\link{plot.lm}} implementation, the residuals here are taken for sqrt(| residuals |) and not the standardized residuals.
 #'
 scale_location_plot <- function(object, ...) {
   x <- fitted(object, report.var = FALSE)
   y <- sqrt(abs(resid(object)))
   plot(y~x, ...)
+  
+}
+
+
+#' Q-Q Plot
+#' 
+#' Q-Q Plot for a \code{\link{blm}} object.
+#' 
+#' @param object a \code{\link{blm}} object.
+#' @param ... other arguments passed to qqplot.
+#'   
+#' @detail Q-Q plots the sqrt(| residuals |) against fitted values. This transformation of the residuals is supposed to diminish skewness (see \code{\link{plot.lm}}. Note: In contrast to the \code{\link{plot.lm}} implementation, the residuals here are taken for sqrt(| residuals |) and not the standardized residuals.
+#'
+qq_blm_plot <- function(object, ...) {
+  x <- fitted(object, report.var = FALSE)
+  y <- resid(object)
+  
+  qqplot(x, y, 
+         main = 'Normal Q-Q',
+         xlab = paste('Theoretical Quantiles\n',
+                      deparse(object$formula),sep=''),
+         ylab = 'Residuals',
+         ...)
   
 }
